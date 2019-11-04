91ea017599b83c725ed19de758946df7a0502ef6
Sebastian Di Luzio <sebidiluzio@gmail.com>
2019-11-04 22:15:57 +0100

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,8 @@
   <v-layout column justify-center align-center>
     <v-flex xs12 sm8 md6>
       <div class="text-center">
-        <logo />
-        <vuetify-logo />
+        <Logo />
+        <VuetifyLogo />
       </div>
       <v-card>
         <v-card-title class="headline">
@@ -63,13 +63,13 @@
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
+import Logo from '~/components/Logo.vue';
+import VuetifyLogo from '~/components/VuetifyLogo.vue';
 
 export default {
   components: {
     Logo,
-    VuetifyLogo
-  }
-}
+    VuetifyLogo,
+  },
+};
 </script>
