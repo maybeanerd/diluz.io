c8a931caef45d46e704367898c07b1b2e126cbcd
Sebastian Di Luzio <sebidiluzio@gmail.com>
2021-04-19 23:26:24 +0200

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -45,11 +45,11 @@
       </v-btn>
     </v-app-bar>
     -->
-    <v-content>
+    <v-main>
       <v-container>
         <nuxt />
       </v-container>
-    </v-content>
+    </v-main>
     <!--
     <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
       <v-list>
