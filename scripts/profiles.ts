f6971ba69c37ce075b121404c4880889d0b1abb5
Marc Di Luzio <marc.diluzio@gmail.com>
2022-01-22 17:08:22 +0000

    Add jobs and projects for Marc

--- scripts/profiles.ts
+++ scripts/profiles.ts
@@ -312,7 +312,133 @@ const marc: profile = {
     ],
   },
   projects: [
-    // TODO
+    {
+      title: "Unity",
+      role: "Lead Software Engineer",
+      url: "https://unity.com/",
+      image: "unity.png",
+      description: "Leading a tools team in Release Engineering, working on a variety of projects focusing on better release workflows, bug tracking and quality barriers.",
+      timeframe: {
+        start: new Date(2021, 12),
+        end: "current",
+      },
+      showInProfile: true,
+      type: projectType.job,
+      technologies: [
+        "ASP.NET",
+        "Kubernetes",
+        "CI/CD",
+      ],
+      highlights: [
+        "Building a team of fantastic developers",
+      ],
+    },
+    {
+      title: "Unity",
+      role: "Senior Software Engineer",
+      url: "https://unity.com/",
+      image: "unity.png",
+      description: "Worked in Sustained Engineering, focusing on Linux bugs and infrastructure.",
+      timeframe: {
+        start: new Date(2018, 4),
+        end: new Date(2021, 12),
+      },
+      showInProfile: true,
+      type: projectType.job,
+      highlights: [
+        "Releasing the Linux Editor out of Preview",
+      ],
+      technologies: [
+        "C/C++/C#",
+        "Vulkan/OpenGL",
+        "CI/CD",
+      ],
+    },
+    {
+      title: "Rove",
+      role: "Developer and Designer",
+      url: "https://github.com/mdiluz/rove",
+      image: "rove.png",
+      description: "An asynchronous nomadic game about exploring as part of a loose community.",
+      timeframe: {
+        start: new Date(2020, 3),
+        end: "current",
+      },
+      showInProfile: true,
+      type: projectType.sideproject,
+      technologies: [
+        "Golang",
+        "protobuf",
+        "GRPC",
+      ],
+    },
+
+    {
+      title: "GameMode",
+      role: "Founding Developer, now Contributor",
+      url: "https://github.com/FeralInteractive/gamemode",
+      description: "An open source operating system optimiser for Linux",
+      timeframe: {
+        start: new Date(2018, 3),
+        end: "current",
+      },
+      showInProfile: true,
+      type: projectType.sideproject,
+      highlights: [
+        "Added as a default package shipped with all Ubuntu Desktop installs",
+      ],
+      technologies: [
+        "C",
+        "SDbus",
+        "Meson",
+        "Inih",
+      ],
+    },
+    {
+      title: "Feral Interactive",
+      role: "Linux Group lead",
+      url: "https://www.feralinteractive.com/en/",
+      image: "feral_interactive.png",
+      description: "Lead the Linux platform development while porting many AAA games such as XCOM2, Mad Max and HITMAN",
+      timeframe: {
+        start: new Date(2013, 8),
+        end: new Date(2015, 5),
+      },
+      showInProfile: true,
+      type: projectType.job,
+      highlights: [
+        "Built a small team of great developers",
+        "Spearheaded Vulkan games development on Linux with Mad Max",
+        "Shipped over 30 titles to Linux",
+      ],
+      technologies: [
+        "C++",
+        "Vulkan/OpenGL",
+      ],
+    },
+    {
+      title: "Feral Interactive",
+      role: "Software Developer",
+      url: "https://www.feralinteractive.com/en/",
+      image: "feral_interactive.png",
+      description: "Worked on Mac and Linux ports for a variety of games including ROME: Total War, Tomb Raider and XCOM",
+      timeframe: {
+        start: new Date(2015, 5),
+        end: new Date(2018, 3),
+      },
+      showInProfile: true,
+      type: projectType.job,
+      highlights: [
+        "Shipped a single executable supporting both ROME and it's standalone expansions on Mac",
+        "Worked on shipping the first Linux game Feral sold",
+      ],
+      technologies: [
+        "C++",
+        "OpenGL",
+        "CMake",
+        "Perl",
+      ],
+    },
   ],
 };
 
