a35f72fa2cc2da5b36bb469892072bfd1d71a946
Sebastian Di Luzio <sebidiluzio@gmail.com>
2022-01-23 23:18:31 +0100

    add pp icons to buttons on main page. make github link a bit clearer

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,16 @@
 <template>
   <v-container fill-height fluid class="container">
     <v-row align="center" justify="center">
-      <v-col class="column"
-        ><v-card class="services">
-          <v-row justify="center">
-            <img
-              class="service"
-              src="~assets/images/services/GitHub/GitHub-Mark-Light-120px-plus.png"
-              @click="gotoGithub"
-            />
-          </v-row>
-        </v-card>
+      <v-col class="column">
         <v-card class="card">
           <v-card-title class="headline justify-center"
-            >Check out the diluz.ios</v-card-title
+            >Check out the diluz.ios:</v-card-title
           >
           <v-card-text>
             <v-row
               v-for="[name, profile] in profiles"
               :key="name"
-              class="pa-2"
+              class="pb-3 pt-1"
               align="center"
               justify="center"
             >
@@ -27,10 +18,28 @@
                 :to="profile.person.name.first.toLowerCase()"
                 class="unstyled-link"
               >
-                <v-btn>{{ profile.person.name.first }}</v-btn>
+                <v-btn
+                  ><img
+                    class="profileIcon mr-2"
+                    :src="getProfilePictureFromProfile(profile)"
+                  />
+                  {{ profile.person.name.first }}</v-btn
+                >
               </nuxt-link>
             </v-row>
           </v-card-text>
+        </v-card>
+        <v-card class="services">
+          <v-row justify="center">
+            <img
+              class="service"
+              src="~assets/images/services/GitHub/GitHub-Mark-Light-120px-plus.png"
+              @click="gotoGithub"
+            />
+          </v-row>
+          <v-card-title class="justify-center pointer" @click="gotoGithub"
+            >View the sourcecode on GitHub</v-card-title
+          >
         </v-card>
       </v-col>
     </v-row>
@@ -43,6 +52,7 @@ import Vue from 'vue';
 import Logo from '~/components/Logo.vue';
 import VuetifyLogo from '~/components/VuetifyLogo.vue';
 import { profiles } from '~/scripts/profiles';
+import { getProfilePictureFromProfile } from '~/scripts/util';
 
 @Component({
   components: {
@@ -56,11 +66,15 @@ export default class homePage extends Vue {
     window.open('https://github.com/T0TProduction/diluz.io', '_blank');
   }
 
+  getProfilePictureFromProfile = getProfilePictureFromProfile;
+
   profiles = profiles;
 }
 </script>
 
 <style lang="scss" scoped>
+@import '~/assets/shared-styles.scss';
+
 .unstyled-link {
   text-decoration: none;
 }
@@ -75,11 +89,15 @@ export default class homePage extends Vue {
   text-align: center;
 }
 .services {
+  margin-top: 2rem;
   background: none;
   box-shadow: none !important;
 }
 .service {
   height: 96px;
+  cursor: pointer;
+}
+.pointer {
   cursor: pointer;
 }
 @media screen and (max-width: 600px) {
