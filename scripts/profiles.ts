d1596e1c306d8ac59c6115f5f009eda77a5c2f06
7crmbs2 <51399053+7crmbs2@users.noreply.github.com>
2022-07-27 21:30:20 +0200

    add julian with pp and not done info

--- scripts/profiles.ts
+++ scripts/profiles.ts
@@ -3,9 +3,10 @@
 import { Profile } from '~/types/CV';
 import { marc } from './profiles/marc';
 import { sebastian } from './profiles/sebastian';
+import { julian } from './profiles/julian';
 
 /* TODO later on i want to build an editor for others to fill in their data,
  *  which exports them a correct json
  */
 
-export const profiles: Array<Profile> = [sebastian, marc];
+export const profiles: Array<Profile> = [sebastian, marc, julian];
