d07dca55e6739652888f83087f63f70a027c0900
Sebastian Di Luzio <sebidiluzio@gmail.com>
2023-02-26 21:29:46 +0100

    test: clean up navbar tests

--- cypress/e2e/profiles.cy.ts
+++ cypress/e2e/profiles.cy.ts
@@ -2,26 +2,57 @@ import { profiles } from '~/scripts/profiles';
 import { runTestForAllViewports } from '~/cypress/support/util';
 
 describe('Profiles', () => {
+  describe('Navbar', () => {
+    it('should be openable and link to all users', () => {
+      const firstName = profiles[// switch to .at when we upgrade to node 18
+        Math.floor(Math.random() * profiles.length)
+      ].person.name.first.toLowerCase();
+
+      runTestForAllViewports(() => {
+        cy.visit(`/${firstName}`);
+
+        cy.findByText('Home').should('be.hidden');
+
+        cy.findByTestId('navbar-hamburger').click();
+
+        cy.findByText('Home').should('be.visible');
+
+        cy.findByText('Home').click();
+
+        cy.url().should('contain', '/');
+
+        cy.findByText('Check out the diluz.ios:');
+
+        profiles.forEach((profileInNavbar) => {
+          cy.visit(`/${firstName}`);
+
+          cy.findByText(profileInNavbar.person.name.first).should('be.hidden');
+          cy.findByTestId('navbar-hamburger').click();
+
+          const firstNameInNavbar = profileInNavbar.person.name.first;
+
+          cy.findByText(firstNameInNavbar).should('be.visible');
+
+          cy.findByText(firstNameInNavbar).click();
+
+          cy.url().should('contain', `/${firstNameInNavbar.toLowerCase()}`);
+
+          if (firstNameInNavbar.toLowerCase() === firstName) {
+            cy.findByText('Home').should('be.visible');
+          } else {
+            cy.findByText('Home').should('be.hidden');
+          }
+        });
+      });
+    });
+  });
+
   profiles.forEach((profile) => {
     const firstName = profile.person.name.first.toLowerCase();
 
     describe(`Profile: ${firstName}`, () => {
-      it('should have the navbar', () => {
+      it('should have the navbar and show the current profile as active', () => {
         runTestForAllViewports(() => {
-          cy.visit(`/${firstName}`);
-
-          cy.findByText('Home').should('be.hidden');
-
-          cy.findByTestId('navbar-hamburger').click();
-
-          cy.findByText('Home').should('be.visible');
-
-          cy.findByText('Home').click();
-
-          cy.url().should('contain', '/');
-
-          cy.findByText('Check out the diluz.ios:');
-
           profiles.forEach((profileInNavbar) => {
             cy.visit(`/${firstName}`);
 
@@ -45,12 +76,6 @@ describe('Profiles', () => {
               .parent()
               .parent()
               .should('not.have.class', 'v-list-item--active');
-
-            cy.findByText(firstNameInNavbar).click();
-
-            cy.url().should('contain', `/${firstNameInNavbar.toLowerCase()}`);
-
-            cy.findByText('Home').should('be.hidden');
           });
         });
       });
