eebd99e04d0ac8e268a398c2c0766224f7cfda80
Sebastian Di Luzio <sebidiluzio@gmail.com>
2023-06-21 21:31:03 +0200

    Update .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,11 +4,20 @@ module.exports = {
     browser: true,
     node: true,
   },
-  parser: "vue-eslint-parser",
+  parser: 'vue-eslint-parser',
   parserOptions: {
-    parser: "@typescript-eslint/parser",
+    parser: '@typescript-eslint/parser',
   },
-  extends: ["@nuxtjs/eslint-config-typescript", "plugin:prettier/recommended"],
+  extends: ['plugin:prettier/recommended', '@nuxtjs/eslint-config-typescript'],
   plugins: [],
-  rules: {},
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+      },
+    ],
+    'comma-dangle': ['error', 'always-multiline'],
+    semi: ['error', 'always'],
+  },
 };
