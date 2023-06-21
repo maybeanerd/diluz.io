8b98dbf5e42e5d16da7889ecc3af1e8cfa44a5ae
Sebastian Di Luzio <sebidiluzio@gmail.com>
2023-06-21 22:49:00 +0200

    create profile page

--- components/profile.vue
+++ components/profile.vue
@@ -1,14 +1,21 @@
 <template>
   <div>
-    <h1>Profile</h1>
-    <div>
-      <div v-for="(profile, index) in profiles" :key="index">
-        {{ profile.person.name }}
-      </div>
+    <h1>Profile of {{ props.name }}</h1>
+    <div v-if="profile !== undefined">
+      {{ profile.projects.list.at(0) }}
+    </div>
+    <div v-else>
+      Profile not found
     </div>
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import { profiles } from '../server/profiles';
+
+const props = defineProps<{name: string}>();
+
+const profile = computed(() => profiles.find(profile =>
+  profile.person.name.first.toLowerCase() === props.name.toLowerCase()),
+);
 </script>
