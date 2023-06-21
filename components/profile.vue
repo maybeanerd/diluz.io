4e48174134b3d084fe185fbc2f2f424a8618a705
Sebastian Di Luzio <sebidiluzio@gmail.com>
2023-06-21 22:51:49 +0200

    Update profile.vue

--- components/profile.vue
+++ components/profile.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>Profile of {{ props.name }}</h1>
-    <div v-if="profile !== undefined">
+    <div v-if="profile !== null">
       {{ profile.projects.list.at(0) }}
     </div>
     <div v-else>
@@ -13,9 +13,19 @@
 <script setup lang="ts">
 import { profiles } from '../server/profiles';
 
+const router = useRouter();
 const props = defineProps<{name: string}>();
 
-const profile = computed(() => profiles.find(profile =>
-  profile.person.name.first.toLowerCase() === props.name.toLowerCase()),
+const profile = computed(() => {
+  const foundProfile = profiles.find(profile =>
+    profile.person.name.first.toLowerCase() === props.name.toLowerCase());
+  if (foundProfile === undefined) {
+    // TODO - redirect to 404 page
+    // TODO - manage this in a router guard and not inside the component
+    router.push('/');
+    return null;
+  }
+  return foundProfile;
+},
 );
 </script>
