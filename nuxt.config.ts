42e59602fe35b348e8931c71cf60164424f38816
Sebastian Di Luzio <sebidiluzio@gmail.com>
2023-06-21 21:41:15 +0200

    support static site generation

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  nitro: {
+    prerender: {
+      crawlLinks: true,
+    },
+  },
 });
