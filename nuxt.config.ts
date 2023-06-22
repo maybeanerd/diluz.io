07e64329dc12484364c8c04a7f7977367a9c0c96
Sebastian Di Luzio <sebidiluzio@gmail.com>
2023-06-22 20:54:48 +0200

    Revert "add a component library and tailwind"
    This reverts commit 99959bff7dbcb9ff6dc448ce713eb2beb9ece6fa.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,39 +1,9 @@
-import { colors } from '@indielayer/ui';
-import icons from './assets/icons';
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
   nitro: {
     prerender: {
       crawlLinks: true,
-    },
-  },
-  modules: [
-    ['@indielayer/ui/nuxt', {
-      icons,
-      prefix: 'X',
-      theme: {
-        classPrefix: 'x-',
-        colors: {
-          primary: colors.emerald,
-          secondary: colors.slate,
-          success: colors.green,
-          warning: colors.yellow,
-          error: colors.red,
-        },
-      },
-    }],
-  ],
-  css: ['~/assets/tailwind.css'],
-  build: {
-    postcss: {
-      postcssOptions: {
-        plugins: {
-          'tailwindcss/nesting': {},
-          tailwindcss: {},
-          autoprefixer: {},
-        },
-      },
     },
   },
 });
