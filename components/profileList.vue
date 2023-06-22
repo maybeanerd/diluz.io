4fe07b54d116a3c751a375d866d2c63ec1a09af5
Sebastian Di Luzio <sebidiluzio@gmail.com>
2023-06-22 21:21:38 +0200

    feat: use naive-ui

--- components/profileList.vue
+++ components/profileList.vue
@@ -1,16 +1,25 @@
 <template>
   <div>
-    <h1>
-      Profile List
-    </h1>
+    <h1>Profile List</h1>
     <div>
       <div v-for="(profile, index) in profiles" :key="index">
         {{ profile.person.name }}
+        <NButton @click="goToProfile(profile)">
+          Go to profile
+        </NButton>
       </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { profiles } from '../server/profiles';
+import { NButton } from 'naive-ui';
+import { profiles } from '~/server/profiles';
+import { Profile } from '~/server/profile.types';
+
+const router = useRouter();
+
+function goToProfile (profile: Profile) {
+  router.push(`/${profile.person.name.first.toLowerCase()}`);
+}
 </script>
