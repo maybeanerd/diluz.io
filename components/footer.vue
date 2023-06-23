0e5a592d4b81e2c12417b6107531a4ea1623ebf3
Sebastian Di Luzio <sebidiluzio@gmail.com>
2023-06-23 21:45:52 +0200

    add commit hash to footer

--- components/footer.vue
+++ components/footer.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="border-2 p-2 text-right">
-    Made with ❤️ and TypeScript, Nuxt, Tailwind CSS, and Naive UI - built on: {{ buildDate }}
+    Made with ❤️ and TypeScript, Nuxt, Tailwind CSS, and Naive UI - version {{ commitHash }}
   </div>
 </template>
 
 <script setup lang="ts">
-const runtimeConfig = useRuntimeConfig();
+import gitCommitInfo from 'git-commit-info';
 
-const buildDate = new Date(runtimeConfig.public.buildDate).toLocaleString('de-DE');
+const commitHash = gitCommitInfo().shortHash;
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -33,6 +33,6 @@ export default defineNuxtConfig({
     },
   },
   runtimeConfig: {
-    public: { buildDate: new Date() },
+    public: {},
   },
 });
