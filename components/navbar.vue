10304cfa36256f6516ad4afb2fdcf3a05ac3b674
Sebastian Di Luzio <sebidiluzio@gmail.com>
2023-06-24 16:25:52 +0200

    use menu component to build menu
    TODO:
    - add pp pics as images/icons
    - make currently active element be somehow marked

--- components/navbar.vue
+++ components/navbar.vue
@@ -1,21 +1,52 @@
 <template>
   <NDrawer v-model:show="isOpen" placement="left">
     <NDrawerContent title="The Di Luzios" closable>
-      <ProfileList />
+      <NMenu :options="menuOptions" @update:value="handleNavbarLinkClicked" />
     </NDrawerContent>
   </NDrawer>
 </template>
 
 <script setup lang="ts">
-import { NDrawer, NDrawerContent } from 'naive-ui';
+import { MenuOption, NDrawer, NDrawerContent, NIcon, NMenu } from 'naive-ui';
+import { Airplane } from '@vicons/ionicons5';
+import { RouterLink } from '~/.nuxt/vue-router';
 import { useMenu } from '~/composables/useMenu';
+import { upperCaseFirstLetter } from '~/utils/string';
+import { profiles } from '~/server/profiles';
 
 const { isOpen } = useMenu();
 
-const router = useRouter();
-
-router.afterEach(() => {
-  // Close navbar on route change
+function handleNavbarLinkClicked () {
+  // Close navbar when it was used
   isOpen.value = false;
+}
+
+// TODO change this to render the image of a profile
+function renderIcon (icon: Component) {
+  return () => h(NIcon, null, { default: () => h(icon) });
+}
+
+const menuOptions = profiles.map((profile) => {
+  const name = profile.person.name.first.toLowerCase();
+
+  const menuOption: MenuOption =
+    {
+      label: () =>
+        h(
+          RouterLink,
+          {
+            to: {
+              path: `/${name}`,
+
+            },
+          },
+          upperCaseFirstLetter(name),
+        ),
+      key: name,
+      icon: renderIcon(Airplane),
+    };
+
+  return menuOption;
 });
+
 </script>
