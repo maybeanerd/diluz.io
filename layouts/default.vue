bc5770fe03224d8fbe46a718324a7ed6b6d4b8b6
Sebastian Di Luzio <sebidiluzio@gmail.com>
2023-06-24 16:19:37 +0200

    fix header being sticky again

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,9 +6,9 @@
     class="h-screen"
   >
     <Navbar />
-    <div class="flex flex-col min-h-full w-screen overflow-x-hidden justify-between">
+    <div class="flex flex-col min-h-full justify-between">
       <Header />
-      <div class="m-4 lg:m-8">
+      <div class="p-4 lg:p-8 w-screen overflow-x-hidden">
         <slot />
       </div>
       <Footer />
