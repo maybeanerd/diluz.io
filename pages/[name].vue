6025a2d80b17e2a9711f4af75805c1bb62fb57ac
Sebastian Di Luzio <sebidiluzio@gmail.com>
2023-06-25 14:05:44 +0200

    add title to navbar, clean up navigation logic a bit

--- pages/[name].vue
+++ pages/[name].vue
@@ -1,8 +1,28 @@
 <template>
-  <Profile :name="name" />
+  <Profile :name="name ?? defaultProfile" />
 </template>
 
-<script setup>
+<script setup lang="ts">
+import { useHeader } from '~/composables/useHeader';
+import { defaultProfile } from '~/constants/defaultProfile';
+import { profiles } from '~/server/profiles';
+
+definePageMeta({
+  validate: (route) => {
+    const { name } = route.params;
+    return (
+      typeof name === 'string' &&
+      profiles.some(
+        profile =>
+          profile.person.name.first.toLowerCase() === name.toLowerCase(),
+      )
+    );
+  },
+});
+
 const route = useRoute();
-const { name } = route.params;
+const name = route.params.name as string;
+
+const { title } = useHeader();
+title.value = name;
 </script>
--- pages/index.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <Profile :name="defaultProfile" />
-</template>
-
-<script setup>
-import { defaultProfile } from '~/constants/defaultProfile';
-</script>
