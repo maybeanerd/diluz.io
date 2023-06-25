e906cdb0429744676a1b13ba10507105c054fcba
Sebastian Di Luzio <sebidiluzio@gmail.com>
2023-06-25 11:44:31 +0200

    move profile.person to its own component

--- components/profile/index.vue
+++ components/profile/index.vue
@@ -2,7 +2,7 @@
   <div>
     <NH1>Profile of {{ props.name }}</NH1>
     <div v-if="profile !== null">
-      {{ profile }}
+      <ProfilePerson :person="profile.person" />
     </div>
     <div v-else>
       Profile not found
@@ -15,11 +15,13 @@ import { NH1 } from 'naive-ui';
 import { profiles } from '~/server/profiles';
 
 const router = useRouter();
-const props = defineProps<{name: string}>();
+const props = defineProps<{ name: string }>();
 
 const profile = computed(() => {
-  const foundProfile = profiles.find(profile =>
-    profile.person.name.first.toLowerCase() === props.name.toLowerCase());
+  const foundProfile = profiles.find(
+    profile =>
+      profile.person.name.first.toLowerCase() === props.name.toLowerCase(),
+  );
   if (foundProfile === undefined) {
     // TODO - redirect to 404 page
     // TODO - manage this in a router guard and not inside the component
@@ -27,6 +29,5 @@ const profile = computed(() => {
     return null;
   }
   return foundProfile;
-},
-);
+});
 </script>
