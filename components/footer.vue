ade4d47b0889f30f5a9850d0f305f934eb8ea696
Sebastian Di Luzio <sebidiluzio@gmail.com>
2023-06-25 14:57:15 +0200

    make dependencies in footer link to their pages

--- components/footer.vue
+++ components/footer.vue
@@ -1,7 +1,31 @@
 <template>
-  <div class="border-2 p-2 text-right">
-    Made with ❤️ and TypeScript, Nuxt, Tailwind CSS, and Naive UI
+  <div class="flex justify-end border-t-[1px] border-gray-700 p-2">
+    Created with&nbsp;
+    <template
+      v-for="(dependency, index) in notableDependencies"
+      :key="dependency.name"
+    >
+      <CustomLink :url="dependency.url">
+        {{ dependency.name }}
+      </CustomLink>{{ index === notableDependencies.length - 1 ? '.' : ',&nbsp;' }}
+    </template>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const notableDependencies = [
+  { name: 'Nuxt', packageName: 'nuxt', url: new URL('https://nuxt.com/') },
+  {
+    name: 'TypeScript',
+    url: new URL('https://www.typescriptlang.org/'),
+  },
+  {
+    name: 'Tailwind CSS',
+    url: new URL('https://tailwindcss.com/'),
+  },
+  {
+    name: 'Naive UI',
+    url: new URL('https://www.naiveui.com/'),
+  },
+];
+</script>
