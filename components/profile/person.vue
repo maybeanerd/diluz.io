ccaefe80cbe0c944df75888a31e25ee86e1821d0
Sebastian Di Luzio <sebidiluzio@gmail.com>
2023-06-25 11:44:31 +0200

    move profile.person to its own component

---
+++ components/profile/person.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <NH1>Person</NH1>
+    <div>{{ props.person }}</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { NH1 } from 'naive-ui';
+
+import type { Profile } from '~/server/profile.types';
+
+const props = defineProps<{ person: Profile['person'] }>();
+</script>
