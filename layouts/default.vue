c347fc804bae1902edd341688d6d182ef710fa66
Sebastian Di Luzio <sebidiluzio@gmail.com>
2023-06-25 22:27:11 +0200

    add back to top button to see if its useful

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,6 +6,7 @@
     class="h-screen"
   >
     <Navbar />
+    <NBackTop :right="20" />
     <div class="flex flex-col min-h-full justify-between">
       <Header />
       <div class="p-4 lg:p-8 w-screen overflow-x-hidden">
@@ -17,5 +18,5 @@
 </template>
 
 <script setup lang="ts">
-import { NConfigProvider, darkTheme, enUS, dateDeDE } from 'naive-ui';
+import { NConfigProvider, darkTheme, enUS, dateDeDE, NBackTop } from 'naive-ui';
 </script>
