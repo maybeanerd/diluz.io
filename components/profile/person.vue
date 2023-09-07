5555eed02fb5afb13b67ff0dbd4d39cd7bc67f9c
Sebastian Di Luzio <sebidiluzio@gmail.com>
2023-09-07 13:17:40 +0200

    clean up person a bit

--- components/profile/person.vue
+++ components/profile/person.vue
@@ -1,23 +1,23 @@
 <template>
   <div>
-    <NH1>{{ props.person.name.first }} {{ props.person.name.last }}</NH1>
-    <div>
-      <div>
-        {{ props.person.shortText }}
-      </div>
+    <NH1>
+      {{ props.person.name.first }} {{ props.person.name.last }}
       <LocationTag v-if="props.person.basedIn" :location="props.person.basedIn" />
-      <SocialTag
-        v-for="social in props.person.socials"
-        :key="social.type"
-        class="m-1"
-        :social="social"
-      />
-    </div>
+    </NH1>
+    <NH3>
+      {{ props.person.shortText }}
+    </NH3>
+    <SocialTag
+      v-for="social in props.person.socials"
+      :key="social.type"
+      class="m-1"
+      :social="social"
+    />
   </div>
 </template>
 
 <script setup lang="ts">
-import { NH1 } from 'naive-ui';
+import { NH1, NH3 } from 'naive-ui';
 
 import type { Profile } from '~/server/profile.types';
 
