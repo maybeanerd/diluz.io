c3eaa42825f513d599dff231fa72c9913d1a33d0
Sebastian Di Luzio <sebidiluzio@gmail.com>
2023-09-07 14:26:01 +0200

    feat: add first draft of project timeline

---
+++ components/profile/projectItem.vue
@@ -0,0 +1,16 @@
+<template>
+  <NTimelineItem
+    type="info"
+    title="Info"
+    :content="JSON.stringify(props.project,null,2)"
+    time="2018-04-03 20:46"
+  />
+</template>
+
+<script setup lang="ts">
+import { NTimelineItem } from 'naive-ui';
+
+import type { Project } from '~/server/profile.types';
+
+const props = defineProps<{ project: Project}>();
+</script>
--- components/profile/projects.vue
+++ components/profile/projects.vue
@@ -1,12 +1,52 @@
 <template>
   <div>
     <NH1>Projects</NH1>
-    <div>{{ props.projects }}</div>
+    <div class="mx-4">
+      <NTimeline>
+        <NTimelineItem content="Oops" />
+        <NTimelineItem
+          type="success"
+          title="Success"
+          content="success content"
+          time="2018-04-03 20:46"
+        />
+        <NTimelineItem
+          type="error"
+          content="Error content"
+          time="2018-04-03 20:46"
+        />
+        <NTimelineItem
+          type="warning"
+          title="Warning"
+          content="warning content"
+          time="2018-04-03 20:46"
+        />
+        <NTimelineItem content="Oops" />
+        <ProfileProjectItem
+          v-for="(project, index) in props.projects.list"
+          :key="index"
+          :project="project"
+        />
+        <template v-if="props.projects.final">
+          <NTimelineItem
+            type="info"
+            :title="props.projects.final.title"
+            :content="props.projects.final.text"
+            line-type="dashed"
+          />
+          <NTimelineItem
+            type="info"
+            title="The beginning of time"
+            :time="new Date(0).toDateString()"
+          />
+        </template>
+      </NTimeline>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { NH1 } from 'naive-ui';
+import { NH1, NTimeline, NTimelineItem } from 'naive-ui';
 
 import type { Profile } from '~/server/profile.types';
 
