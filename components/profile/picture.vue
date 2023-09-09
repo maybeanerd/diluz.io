55f76ec8dce724940e377f1cebfedd8d46ca5a81
Sebastian Di Luzio <sebidiluzio@gmail.com>
2023-09-09 22:57:31 +0200

    refactor: make profilePicture component a bit nicer

--- components/profile/picture.vue
+++ components/profile/picture.vue
@@ -1,16 +1,16 @@
 <template>
   <NuxtPicture
-    v-if="props.person.image"
     sizes="sm:64px md:96px lg:128px"
     class="[&>img]:rounded-full [&>img]:border-white [&>img]:border-[1px] [&>img]:w-16 [&>img]:sm:w-24 [&>img]:md:w-32"
-    :src="`/profile/${props.person.name.first.toLowerCase()}/${
-      props.person.image.path
-    }`"
+    :src="`/profile/${props.name.toLowerCase()}/${props.image.path}`"
   />
 </template>
 
 <script setup lang="ts">
 import type { Profile } from '~/server/profile.types';
 
-const props = defineProps<{ person: Profile['person'] }>();
+const props = defineProps<{
+  image: NonNullable<Profile['person']['image']>;
+  name: string;
+}>();
 </script>
