8ee3f6ac40f7144b823ee9f850a3f80604018b86
Sebastian Di Luzio <sebidiluzio@gmail.com>
2023-09-10 12:05:47 +0200

    refactor: move image path definition to computed

--- components/profile/project/picture.vue
+++ components/profile/project/picture.vue
@@ -2,14 +2,17 @@
   <NuxtPicture
     sizes="sm:64px md:96px lg:128px"
     class="[&>img]:w-16 [&>img]:sm:w-24 [&>img]:md:w-32"
-    :src="`/profile/${props.name.toLowerCase()}/${props.imagePath}`"
+    :src="imagePath"
   />
 </template>
 
 <script setup lang="ts">
-
 const props = defineProps<{
   name: string;
   imagePath: string;
 }>();
+
+const imagePath = computed(() => {
+  return `/profile/${props.name.toLowerCase()}/${props.imagePath}`;
+});
 </script>
