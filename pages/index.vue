7863d7aabf37a2a497c070c83714708a230b1dda
Sebastian Di Luzio <sebidiluzio@gmail.com>
2023-09-10 11:14:18 +0200

    refactor: deal with default profile by not redirecting

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <Profile :profile="profile" />
+</template>
+
+<script setup lang="ts">
+import { useHeader } from '~/composables/useHeader';
+import { defaultProfile } from '~/constants/defaultProfile';
+import { profiles } from '~/server/profiles';
+
+const profile = profiles.find(
+  searchedProfile => searchedProfile.person.name.first.toLowerCase() === defaultProfile,
+)!;
+
+const { title } = useHeader();
+title.value = `${profile?.person.name.first} ${profile?.person.name.last}`;
+</script>
