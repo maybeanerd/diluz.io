08cbb782b4b40b2ce967741878803ba714397dab
Sebastian Di Luzio <sebidiluzio@gmail.com>
2023-09-12 20:00:34 +0200

    refactor: use single composable to get current profile

---
+++ composables/useProfileFromRoute.ts
@@ -0,0 +1,61 @@
+import { defaultProfile } from '~/constants/defaultProfile';
+import { Profile } from '~/server/profile.types';
+import { profiles } from '~/server/profiles';
+
+const profile = ref<Profile>(defaultProfile);
+
+function setMetaInfo (person: Profile['person']) {
+  const pageTitle = `${person.name.first} ${person.name.last}`;
+  const pageDescription = person.shortText ?? `${pageTitle}s personal page.`;
+
+  const { getImage } = useImage();
+
+  const pageImage = person.image
+    ? getImage(`/profile/${person.name.first.toLowerCase()}/${person.image}`)
+      .url
+    : undefined;
+
+  useSeoMeta({
+    title: pageTitle,
+    ogTitle: pageTitle,
+    description: pageDescription,
+    ogDescription: pageDescription,
+    ogImage: pageImage,
+    twitterCard: 'summary_large_image',
+  });
+
+  useHead({
+    link: [
+      {
+        rel: 'icon',
+        type: 'image/png',
+        href: pageImage,
+      },
+    ],
+  });
+}
+
+function setProfileByName (name: string) {
+  console.log('useProfileFromRoute', name);
+  if (name === unref(profile).person.name.first.toLowerCase()) {
+    return;
+  }
+
+  const foundProfile = profiles.find(
+    searchedProfile =>
+      searchedProfile.person.name.first.toLowerCase() === name.toLowerCase(),
+  );
+  if (foundProfile) {
+    profile.value = foundProfile;
+    setMetaInfo(profile.value.person);
+  }
+}
+
+export function useProfileFromRoute () {
+  const route = useRoute();
+  setProfileByName(route.params.name as string);
+
+  return {
+    profile,
+  };
+}
