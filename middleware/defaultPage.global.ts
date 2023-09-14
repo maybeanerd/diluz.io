32b4bfcaa8258b1445abf80e37ab3094cdc3b029
Sebastian Di Luzio <sebidiluzio@gmail.com>
2023-09-14 11:11:01 +0200

    refactor: handle default profile by actually handling route without redirect -> this should also handle meta data for root path correctly in SSR now

--- middleware/defaultPage.global.ts
+++
@@ -1,8 +0,0 @@
-import { defaultProfileName } from '~/server/profiles';
-
-export default defineNuxtRouteMiddleware((to /* , from */) => {
-  // Default to the default profile if no profile is specified
-  if (to.path === '/') {
-    return navigateTo(`/${defaultProfileName}`);
-  }
-});
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <Profile :profile="profile" />
+</template>
+
+<script setup lang="ts">
+// This page uses the default profile
+const { profile } = useProfileFromRoute();
+</script>
