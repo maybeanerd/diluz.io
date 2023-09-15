1cc03516a80f16e27aa7e362e43818f91b671e54
Sebastian Di Luzio <sebidiluzio@gmail.com>
2023-09-15 13:06:11 +0200

    fix: remove NaiveUI mobile&tablet theme to have no layout shifts anymore

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,5 @@
 <template>
-  <NaiveConfig
-    :theme-config="themeConfig"
-    class="h-screen"
-  >
+  <NaiveConfig :theme-config="themeConfig" class="h-screen">
     <Navbar />
     <NBackTop :right="20" />
     <div class="flex flex-col min-h-full justify-between">
@@ -23,61 +20,6 @@ const { colorModePreference } = useNaiveColorMode();
 colorModePreference.set('system');
 
 const { theme } = tailwindConfig;
-
-// We don't want to have a mobile "theme" at all,
-// since all it really does is introduce layout shift after loading the page
-// This fallback makes sure the theme looks like the normal desktop one does
-// might solve this with a feature later as mentioned here https://github.com/becem-gharbi/nuxt-naiveui/issues/21
-const themeFallback = {
-  common: {
-    fontSize: '14px',
-    heightMedium: '34px',
-    fontSizeMedium: '14px',
-  },
-  Form: {
-    labelFontSizeTopMedium: '14px',
-  },
-  Input: {
-    heightMedium: '34px',
-    fontSizeMedium: '14px',
-  },
-  Button: {
-    heightMedium: '34px',
-    fontSizeMedium: '14px',
-  },
-  Card: {
-    fontSizeMedium: '14px',
-  },
-  Avatar: {
-    heightMedium: '34px',
-    fontSize: '14px',
-  },
-  ColorPicker: {
-    heightMedium: '34px',
-    fontSizeMedium: '14px',
-  },
-  Dropdown: {
-    optionHeightMedium: '34px',
-    fontSizeMedium: '14px',
-  },
-  Radio: {
-    buttonHeightMedium: '34px',
-    fontSizeMedium: '14px',
-  },
-  Skeleton: {
-    heightMedium: '34px',
-  },
-  Tag: {
-    heightMedium: '34px',
-    fontSizeMedium: '14px',
-  },
-  Result: {
-    fontSizeMedium: '14px',
-  },
-  Tabs: {
-    tabFontSizeMedium: '14px',
-  },
-};
 
 // TODO finalize theme
 const themeConfig: ThemeConfig = {
@@ -101,7 +43,7 @@ const themeConfig: ThemeConfig = {
       primaryColorPressed: theme.colors.primary.action.dark,
     },
   },
-  mobile: themeFallback,
-  mobileOrTablet: themeFallback,
+  mobile: { defaults: false },
+  mobileOrTablet: { defaults: false },
 };
 </script>
