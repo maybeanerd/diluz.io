cf51c25dc2941dcca87d891384d4ef0304e43f3b
Sebastian Di Luzio <sebastian@diluz.io>
2024-09-05 21:17:48 +0200

    wip: remove naive ui
    - configure tailwind to work without it
    - remove color picker, use system theme

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <NaiveConfig :theme-config="themeConfig" class="h-screen">
+  <div class="h-screen">
     <Navbar />
     <NBackTop :right="20" />
     <div class="flex flex-col min-h-full justify-between">
@@ -9,41 +9,9 @@
       </div>
       <Footer />
     </div>
-  </NaiveConfig>
+  </div>
 </template>
 
 <script setup lang="ts">
-import type { ThemeConfig } from '@bg-dev/nuxt-naiveui';
-import tailwindConfig from '#tailwind-config';
 
-const { colorModePreference } = useNaiveColorMode();
-colorModePreference.set('system');
-
-const { theme } = tailwindConfig;
-
-// TODO finalize theme
-const themeConfig: ThemeConfig = {
-  shared: {
-    common: {
-      fontFamily: theme.fontFamily.sans.join(', '),
-      lineHeight: theme.lineHeight.normal,
-    },
-  },
-  light: {
-    common: {
-      primaryColor: theme.colors.primary.base.light,
-      primaryColorHover: theme.colors.primary.highlight.light,
-      primaryColorPressed: theme.colors.primary.action.light,
-    },
-  },
-  dark: {
-    common: {
-      primaryColor: theme.colors.primary.base.dark,
-      primaryColorHover: theme.colors.primary.highlight.dark,
-      primaryColorPressed: theme.colors.primary.action.dark,
-    },
-  },
-  mobile: { defaults: false },
-  mobileOrTablet: { defaults: false },
-};
 </script>
