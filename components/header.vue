cd84b24da85b218f07077806914c69e004c5fc51
Sebastian Di Luzio <sebastian@diluz.io>
2024-09-06 21:08:02 +0200

    fix: language picker styling

--- components/header.vue
+++ components/header.vue
@@ -1,9 +1,10 @@
 <template>
   <div
-    class="p-2 sticky top-0 flex justify-between z-30 bg-background-light dark:bg-background-dark border-b-[1px] border-content-secondary-light dark:border-content-secondary-dark"
+    class="p-2 pr-4 sticky top-0 flex justify-between items-center z-30 bg-background-light dark:bg-background-dark border-b-[1px] border-content-secondary-light dark:border-content-secondary-dark"
   >
     <UButton
       variant="solid"
+      color="white"
       square
       icon="i-heroicons:bars-3-20-solid"
       size="lg"
