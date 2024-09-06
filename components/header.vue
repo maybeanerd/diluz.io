cc91aab1782aaff9111e2d9a0730519c7c52e32b
Sebastian Di Luzio <sebastian@diluz.io>
2024-09-06 21:36:58 +0200

    feat: add color switcher

--- components/header.vue
+++ components/header.vue
@@ -3,17 +3,18 @@
     class="p-2 pr-4 sticky top-0 flex justify-between items-center z-30 bg-background-light dark:bg-background-dark border-b-[1px] border-content-secondary-light dark:border-content-secondary-dark"
   >
     <UButton
-      variant="solid"
+      variant="ghost"
       color="white"
-      square
       icon="i-heroicons:bars-3-20-solid"
-      size="lg"
       @click="toggleMenu"
     />
     <div class="self-center text-lg">
-      <p>{{ profile.person.name.first }}</p>
+      <p>diluz.io</p>
     </div>
-    <LanguagePicker />
+    <div class="flex gap-2">
+      <LanguagePicker />
+      <ColorModePicker />
+    </div>
   </div>
 </template>
 
@@ -21,7 +22,6 @@
 import { useMenu } from '~/composables/useMenu';
 
 const { isOpen } = useMenu();
-const { profile } = useProfileFromRoute();
 
 function toggleMenu () {
   isOpen.value = !isOpen.value;
