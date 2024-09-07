3926ebb1488201c0f482adb579f453ebf4032edf
Sebastian Di Luzio <sebastian@diluz.io>
2024-09-07 14:24:22 +0200

    fix: explicitly define more behavior for i18n; add non-prefixed routes to prerender routes

--- config/i18n.ts
+++ config/i18n.ts
@@ -15,14 +15,15 @@ export const locales: Array<{ code: string, file: `${string}.json`, name: string
 
 export type Locales = 'en' | 'de';
 
-const defaultLocale = 'en';
+export const defaultLocale = 'en';
 
 export const i18n: NuxtConfig['i18n'] = {
   locales,
-  strategy: 'prefix',
+  strategy: 'prefix_and_default',
   detectBrowserLanguage: {
     fallbackLocale: defaultLocale,
   },
   langDir: 'locales',
   defaultLocale,
+  vueI18n: './config/i18n.config.ts',
 };
