5af4c539fc748144a8d8b9b517da660176c710b2
Sebastian Di Luzio <sebastian@diluz.io>
2024-09-07 14:58:00 +0200

    fix: language switcher

--- config/i18n.ts
+++ config/i18n.ts
@@ -19,7 +19,7 @@ export const defaultLocale = 'en';
 
 export const i18n: NuxtConfig['i18n'] = {
   locales,
-  strategy: 'prefix_and_default',
+  strategy: 'prefix',
   detectBrowserLanguage: {
     fallbackLocale: defaultLocale,
   },
