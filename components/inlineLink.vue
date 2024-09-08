f796692444ec4af300570c0fab174b8000d5e3dc
Sebastian Di Luzio <sebastian@diluz.io>
2024-09-08 21:11:41 +0200

    feat: default inline links to show hostname of target; use in timeline items to make sure the links dont wrap and break layouting and co

--- components/inlineLink.vue
+++ components/inlineLink.vue
@@ -3,10 +3,16 @@
     :to="props.to"
     class="text-base-light hover:text-highlight-light dark:text-base-dark dark:hover:text-highlight-dark"
   >
-    <slot />
+    <slot>
+      <span>{{ shortLink }}</span>
+    </slot>
   </CustomRouterLink>
 </template>
 
 <script setup lang="ts">
 const props = defineProps<{ to: URL | string }>();
+
+const shortLink = computed(() => {
+  return new URL(props.to).hostname;
+});
 </script>
